19246738b89f86953184eeb8f0c06f4ff9107f60
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-03-18 01:32:02 +0600

    loading toogle added

--- src/Component/Users/Users.js
+++ src/Component/Users/Users.js
@@ -8,19 +8,35 @@ const Users = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://randomuser.me/api/?results=15")
       .then((res) => res.json())
       .then((data) => {
         setUsers(data.results);
-        setLoading(true);
+        setLoading(false);
       });
   }, []);
 
   return (
-    <div className="users">
-      {users.map((user) => (
-        <User user={user} key={user.cell} />
-      ))}
+    <div>
+      {loading ? (
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <img width={"400px"} src={logo} alt="" />
+          <h2 style={{ textAlign: "center" }}>loading . . .</h2>
+        </div>
+      ) : (
+        <div className="users">
+          {users.map((user) => (
+            <User user={user} key={user.cell} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
