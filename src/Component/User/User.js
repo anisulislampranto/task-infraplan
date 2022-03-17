4807c1da9b7643f585d6c1780b889490b46c7a2a
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-03-18 01:08:28 +0600

    responsive design added && overflow issue solved

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -4,19 +4,26 @@ import "./User.css";
 const User = ({ user }) => {
   console.log(user);
   return (
-    <div className="user">
-      <img className="image" src={user.picture.medium} alt="" />
-      <div>
-        <h3>
-          {user.name.title} {user.name.first} {user.name.last}
-        </h3>
-        <p>
-          <strong>{user.email}</strong>
-        </p>
-        <p></p>
-        <p></p>
+    <div style={{ display: "flex" }}>
+      <div className="user">
+        <img className="image" src={user.picture.medium} alt="" />
+        <div style={{ width: "100%" }}>
+          <h3 style={{ width: "100%" }}>
+            {user.name.title} {user.name.first} {user.name.last}
+          </h3>
+          <p>
+            <strong>{user.email}</strong>
+          </p>
+          <p>
+            {user.location.street.number} {user.location.street.name}
+            {user.location.state.city} {user.location.state.country}
+            {user.location.state.postcode}
+          </p>
+        </div>
       </div>
-      <span>{user.nat}</span>
+      <span style={{ color: "grey", marginLeft: "-35px", marginTop: "20px" }}>
+        {user.nat}
+      </span>
     </div>
   );
 };
--- src/Component/Users/Users.js
+++ src/Component/Users/Users.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import User from "../User/User";
+import "./Users.css";
+import logo from "../../logo.svg";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=15")
       .then((res) => res.json())
-      .then((data) => setUsers(data.results));
+      .then((data) => {
+        setUsers(data.results);
+        setLoading(true);
+      });
   }, []);
 
   return (
